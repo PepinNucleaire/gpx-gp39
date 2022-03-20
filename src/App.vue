ba65c474b4276dfbf38133b920ad42a29e85d067
Julien MULLER <julien.muller@braincube.com>
2022-03-20 01:01:53 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,104 @@
+<script setup>
+import gpx from "gpx-parser-builder";
+</script>
+
+<template>
+  <main>
+    <h1>OpenCPN GPX to GP39 gpx converter</h1>
+    <p>Upload your GPX file from OpenCPN</p>
+    <p>
+      It will automatically transform to a gpx file that can be uploaded to gp39
+    </p>
+    <input type="file" ref="file" @change="readFile()" />
+    <button @click="submitFile">Upload!</button>
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    file: "";
+    gpx: {
+    }
+  },
+  methods: {
+    readFile() {
+      this.file = this.$refs.file.files[0];
+    },
+    submitFile() {
+      const filename = this.file.name;
+      let reader = new FileReader();
+      reader.onload = (e) => {
+        let text = e.target.result;
+        const gpxParsed = gpx.parse(text);
+        console.log(gpxParsed.wpt[0]);
+        let newwpts = [];
+        gpxParsed.wpt.map((wpt) => {
+          const newwpt = {
+            $: {
+              lon: wpt["$"].lon,
+              lat: wpt["$"].lat,
+            },
+            name: wpt.name,
+            extensions: {
+              GP39Symbol: 0,
+              GP39Flag: 1,
+              GP39Comment: "",
+              GP39Color: 1,
+            },
+          };
+          newwpts.push(newwpt);
+        });
+        const newGpx = new gpx({
+          $: gpxParsed["$"],
+          metadata: gpxParsed.metadata,
+          wpt: newwpts,
+        });
+        console.log(newGpx);
+        this.gpx = newGpx;
+      };
+      reader.readAsText(this.file);
+      this.saveFile(this.gpx.toString(), filename);
+    },
+
+    saveFile: function (saving, name) {
+      const data = saving;
+      const blob = new Blob([data], { type: "text/plain" });
+      const e = document.createEvent("MouseEvents"),
+        a = document.createElement("a");
+      a.download = name + "_GP39.gpx";
+      a.href = window.URL.createObjectURL(blob);
+      a.dataset.downloadurl = ["text/gpx", a.download, a.href].join(":");
+      e.initEvent(
+        "click",
+        true,
+        false,
+        window,
+        0,
+        0,
+        0,
+        0,
+        0,
+        false,
+        false,
+        false,
+        false,
+        0,
+        null
+      );
+      a.dispatchEvent(e);
+    },
+  },
+};
+</script>
+<style>
+@import url("simpledotcss/simple.min.css");
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+</style>
