87fec68c2f094e4dbd043b2893381ffebb9c71e4
Julien MULLER <julien.muller@braincube.com>
2022-03-25 10:01:04 +0100

    add link and remove pinia

--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-
-app.mount('#app')
+app.mount("#app");
