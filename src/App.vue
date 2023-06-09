a3561e78f6cea258d70363cd0cb904eaa5828749
Julien MULLER <julien.muller@braincube.com>
2023-06-09 15:43:02 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -6,25 +6,12 @@ import gpx from "./gpx-parser-builder";
   <main>
     <h1>
       OpenCPN GPX to GP39 gpx converter
-      <a
-        href="https://github.com/PepinNucleaire/gpx-gp39"
-        aria-label="Repo source"
-        class="footer-octicon"
-        title="GitHub"
-      >
-        <svg
-          aria-hidden="true"
-          class="octicon octicon-mark-github"
-          height="24"
-          version="1.1"
-          viewBox="0 0 16 16"
-          width="24"
-        >
-          <path
-            fill="white"
-            fill-rule="evenodd"
-            d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"
-          ></path>
+      <a href="https://github.com/PepinNucleaire/gpx-gp39" aria-label="Repo source" class="footer-octicon" title="GitHub">
+        <svg aria-hidden="true" class="octicon octicon-mark-github" height="24" version="1.1" viewBox="0 0 16 16"
+          width="24">
+          <path fill="white" fill-rule="evenodd"
+            d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z">
+          </path>
         </svg>
       </a>
     </h1>
@@ -33,21 +20,36 @@ import gpx from "./gpx-parser-builder";
     <p>
       It will automatically transform to a gpx file that can be uploaded to gp39
     </p>
-    <input type="file" ref="file" @change="readFile()" />
-    <button @click="submitFile">Upload!</button>
+    <input type="file" ref="file" @change="readFile" />
+    <button v-if="loading" @click="submitFile">
+      <div class="lds-ring">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </button>
+
+    <button v-if="!loading" @click="submitFile">
+      Download file for GP39
+    </button>
   </main>
 </template>
 
 <script>
 export default {
   data() {
-    file: "";
-    gpx: {
+    return {
+      file: "",
+      loading: false,
+      gpx: {
+      }
     }
   },
   methods: {
-    readFile() {
-      this.file = this.$refs.file.files[0];
+    readFile(e) {
+      console.log(e)
+      this.file = e.target.files[0];
     },
     checkColor(sym) {
       if (sym == "Symbol-Pin-Black") {
@@ -90,7 +92,14 @@ export default {
         const gpxParsed = gpx.parse(text);
         console.log(gpxParsed.wpt[0]);
         let newwpts = [];
+        let i = 0
         gpxParsed.wpt.map((wpt) => {
+
+          if (!wpt.name) {
+            wpt.name = `z${('0000000' + i).slice(-7)}`
+            i += 1
+          }
+
           const newwpt = {
             $: {
               lon: wpt["$"].lon,
@@ -117,9 +126,10 @@ export default {
         });
         console.log(newGpx);
         this.gpx = newGpx;
+        console.log(this.gpx.toString())
+        this.saveFile(this.gpx.toString(), filename);
       };
       reader.readAsText(this.file);
-      this.saveFile(this.gpx.toString(), filename);
     },
 
     saveFile: function (saving, name) {
@@ -161,5 +171,47 @@ export default {
   padding: 2rem;
 
   font-weight: normal;
+}
+
+.lds-ring {
+  display: inline-block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+}
+
+.lds-ring div {
+  box-sizing: border-box;
+  display: block;
+  position: absolute;
+  width: 64px;
+  height: 64px;
+  margin: 8px;
+  border: 8px solid #fff;
+  border-radius: 50%;
+  animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+  border-color: #fff transparent transparent transparent;
+}
+
+.lds-ring div:nth-child(1) {
+  animation-delay: -0.45s;
+}
+
+.lds-ring div:nth-child(2) {
+  animation-delay: -0.3s;
+}
+
+.lds-ring div:nth-child(3) {
+  animation-delay: -0.15s;
+}
+
+@keyframes lds-ring {
+  0% {
+    transform: rotate(0deg);
+  }
+
+  100% {
+    transform: rotate(360deg);
+  }
 }
 </style>
