1d0c5ca9982c2a1819500a638a66dc4330402cfb
Julien MULLER <julien.muller@braincube.com>
2024-01-04 11:45:10 +0100

    change color and sym

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,25 @@ import gpx from "./gpx-parser-builder";
   <main>
     <h1>
       OpenCPN GPX to GP39 gpx converter
-      <a href="https://github.com/PepinNucleaire/gpx-gp39" aria-label="Repo source" class="footer-octicon" title="GitHub">
-        <svg aria-hidden="true" class="octicon octicon-mark-github" height="24" version="1.1" viewBox="0 0 16 16"
-          width="24">
-          <path fill="white" fill-rule="evenodd"
-            d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z">
-          </path>
+      <a
+        href="https://github.com/PepinNucleaire/gpx-gp39"
+        aria-label="Repo source"
+        class="footer-octicon"
+        title="GitHub"
+      >
+        <svg
+          aria-hidden="true"
+          class="octicon octicon-mark-github"
+          height="24"
+          version="1.1"
+          viewBox="0 0 16 16"
+          width="24"
+        >
+          <path
+            fill="white"
+            fill-rule="evenodd"
+            d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"
+          ></path>
         </svg>
       </a>
     </h1>
@@ -30,9 +43,7 @@ import gpx from "./gpx-parser-builder";
       </div>
     </button>
 
-    <button v-if="!loading" @click="submitFile">
-      Download file for GP39
-    </button>
+    <button v-if="!loading" @click="submitFile">Download file for GP39</button>
   </main>
 </template>
 
@@ -42,47 +53,46 @@ export default {
     return {
       file: "",
       loading: false,
-      gpx: {
-      }
-    }
+      gpx: {},
+    };
   },
   methods: {
     readFile(e) {
-      console.log(e)
+      console.log(e);
       this.file = e.target.files[0];
     },
     checkColor(sym) {
       if (sym == "Symbol-Pin-Black") {
-        return { color: 0, sym: 1 };
+        return { color: 2, sym: 9 };
       }
       if (sym == "Symbol-Exclamation-Magenta") {
-        return { color: 5, sym: 2 };
+        return { color: 3, sym: 0 };
       }
       if (sym == "Sea-DepthB-Yellowk") {
-        return { color: 2, sym: 2 };
+        return { color: 6, sym: 2 };
       }
       if (sym == "triangle") {
         return { color: 5, sym: 1 };
       }
       if (sym == "square") {
-        return { color: 0, sym: 2 };
+        return { color: 4, sym: 1 };
       }
       if (sym == "circle") {
-        return { color: 6, sym: 0 };
+        return { color: 0, sym: 1 };
       }
       if (sym == "xmgreen") {
-        return { color: 3, sym: 0 };
+        return { color: 2, sym: 2 };
       }
-      if (sym == "xmyellow") {
-        return { color: 2, sym: 0 };
+      if (sym == "Symbol-X-Small-Yellow") {
+        return { color: 1, sym: 2 };
       }
       if (sym == "xmred") {
-        return { color: 1, sym: 0 };
+        return { color: 0, sym: 0 };
       }
       if (sym == "xmblue") {
-        return { color: 3, sym: 1 };
+        return { color: 2, sym: 1 };
       }
-      return { color: 0, sym: 0 };
+      return { color: 6, sym: 0 };
     },
     submitFile() {
       const filename = this.file.name;
@@ -92,12 +102,11 @@ export default {
         const gpxParsed = gpx.parse(text);
         console.log(gpxParsed.wpt[0]);
         let newwpts = [];
-        let i = 0
+        let i = 0;
         gpxParsed.wpt.map((wpt) => {
-
           if (!wpt.name) {
-            wpt.name = `z${('0000000' + i).slice(-7)}`
-            i += 1
+            wpt.name = `z${("0000000" + i).slice(-7)}`;
+            i += 1;
           }
 
           const newwpt = {
@@ -126,7 +135,7 @@ export default {
         });
         console.log(newGpx);
         this.gpx = newGpx;
-        console.log(this.gpx.toString())
+        console.log(this.gpx.toString());
         this.saveFile(this.gpx.toString(), filename);
       };
       reader.readAsText(this.file);
